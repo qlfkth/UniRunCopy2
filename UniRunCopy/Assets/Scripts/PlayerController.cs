08e16bcd240f0afdb10c1bb61461dd6ab5a5d223
한비 <hanqi1417@gmail.com>
2022-07-22 10:49:19 +0900

    20220722

--- UniRunCopy/Assets/Scripts/PlayerController.cs
+++ UniRunCopy/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
 
 
 
-    
+
     void Start()
     {
         //게임 오브젝트로부터 사용할 ㅅ컴포넌트들을 가져와 변수에 할당
@@ -28,9 +28,35 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    void Update()
+    private void Update()                
     {
         //사용자 입력을 감지하고 점프하는 처리
+        if (isDead)
+        {
+            //사망 시 처리르 더 이상 진행하지 않고 종료
+            return;
+        }
+        //마우스 왼쪽 버튼을 눌렀으며&&최대 점프 횟수(2)에 도달하지 않았다면
+        if (Input.GetMouseButton(0) && jumpCount < 2)
+        {
+            //점프 횟수 증가
+            jumpCount++;
+            //점프 직전에 속도를 순간적으로 제로(0,0)로 변경
+            playerRigidbdy.velocity = Vector2.zero;
+            //리지드바디에 위쪽으로 힘 주기
+            playerRigidbdy.AddForce(new Vector2(0, jumpForce));
+            //오디오 소스 재생
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbdy.velocity.y > 0)
+        {
+            //마우스 왼쪽 버튼에서 손으 떼는 순간 && 속도의 y 갑이 양수라면(위로 상승 중)
+            //현재 속도를 절반으로 변경
+            playerRigidbdy.velocity = playerRigidbdy.velocity * 0.5f;
+           
+        }
+        //애니메이터의 Grounded 파라미터를 isGounded 값으로 갱신
+        animator.SetBool("Grounded", isGrounded);
     }
 
     private void Die()
