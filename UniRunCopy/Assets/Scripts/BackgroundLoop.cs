a034cc8195c978d1ef73193316fe0561f5e97d62
한비 <hanqi1417@gmail.com>
2022-08-08 10:47:51 +0900

    20220808

---
+++ UniRunCopy/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width;//배경의 가로 길이
+
+    private void Awake()
+    {
+        //가로 길이를 측정하는 처리
+    }
+
+    
+    void Update()
+    {
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을 때 위치르 재배치
+    }
+
+    //위치를 재배치하는 메서드
+    private void Reposition()
+    {
+
+    }
+
+}
