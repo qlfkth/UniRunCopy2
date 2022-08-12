e019ae315cbf1b4e1da989b86134468228314532
한비 <hanqi1417@gmail.com>
2022-08-12 10:46:31 +0900

    20220812

---
+++ UniRunCopy/Assets/Scripts/Chapter5.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     for(int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j <= i; j++) 
+            {
+                Debug.Log("*");
+            }
+        }
+
+
+
+    }
+}
+
+
+
