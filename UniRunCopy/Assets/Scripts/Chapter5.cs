46af77f8323134b03ef791dbeea8842e2ecc1707
한비 <hanqi1417@gmail.com>
2022-08-18 10:49:49 +0900

    20220818

--- UniRunCopy/Assets/Scripts/Chapter5.cs
+++ UniRunCopy/Assets/Scripts/Chapter5.cs
@@ -7,13 +7,10 @@ public class Chapter5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] Score = new int[] { 10, 20, 30, 40, 50 };
 
-        foreach (int a in [5]/5)
-        {
-            Debug.Log(a);
-        }
     }
+   
+    
    
 }
 
--- UniRunCopy/Assets/Scripts/Platform.cs
+++ UniRunCopy/Assets/Scripts/Platform.cs
@@ -17,19 +17,20 @@ public class Platform : MonoBehaviour
         //장애물의 수만큼 루프
         for(int i = 0; i < obstacles.Length; i++)
         {
-            //현재 순번의 장애물을 1/3의 확률로 활성화
-            if (Random.Range(0, 3) == 0)
-            {
-                obstacles[i].SetActive(true);
-            }
-            else
-            {
-                obstacles[i].SetActive(false);
-            }
+            obstacles[i].SetActive(Random.Range(0, 3) == 0 ? true : false);
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        //플레이어 캐릭터가 자신을 밟았을 때 점수를 추가하는 처리
+        //충돌한 상대방의 태그가 Player이고 이전에 플레이어 캐릭터가 밟지 않았다면
+
+        if (collision.collider.tag == "Player" && !stepped)
+        {
+            //점수를 추가하고 밟힘 상태를 참으로 변경
+            stepped = true;
+            GameManager.instane.AddScore(1);
+        }
+        
+
     }
 }
